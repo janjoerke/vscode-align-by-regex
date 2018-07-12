bae7579ce0d0306905a0a5d46cfa6a29125ec6c7
Jan Jörke <jan.joerke@heidelberg.com>
2018-07-12 08:55:04 +0200

    Improvements to parsgin algorithm

--- src/extension.ts
+++ src/extension.ts
@@ -14,37 +14,56 @@ export function activate(context: vscode.ExtensionContext) {
                 let textDocument = textEditor.document;
                 let range = new vscode.Range(new vscode.Position(selection.start.line, 0), new vscode.Position(selection.end.line, textDocument.lineAt(selection.end.line).range.end.character));
                 let text = textDocument.getText(range);
-                let lines = text.split('\n');
+                let lines = text.split('\r\n');
                 let regex = new RegExp(input, 'g');
-                let indexes : number[][] = [];
-                let matchedTexts : string[][] = [];
-                let plainTexts : string[][] = [];
+                let regexIndexes : number[][] = [];
+                let regexs : string[][] = [];
+                let textIndexes : number[][] = [];
+                let texts : string[][] = [];
                 for(let i = 0; i < lines.length; i++) {
                     let line = lines[i];
-                    indexes[i] = [];
-                    matchedTexts[i] = [];
+                    regexs[i] = [];
+                    regexIndexes[i] = [];
+                    texts[i] = [];
+                    textIndexes[i] = [];
                     let result;
                     while ((result = regex.exec(line)) !== null) {
-                        indexes[i].push(regex.lastIndex);
-                        matchedTexts[i].push(result[0]);
+                        regexIndexes[i].push(regex.lastIndex - 1);
+                        regexs[i].push(result[0]);
                     }
-                    plainTexts[i] = [];
-                    if(indexes[i].length > 0) {
-                        plainTexts[i].push(line.substring(0, indexes[i][0] - 1));
-                        for(let j = 0; j < indexes[i].length - 1; j++) {
-                            plainTexts[i].push(line.substring(indexes[i][j] + matchedTexts[i][j].length - 1, indexes[i][j+1] - 1));
+
+                    if(regexIndexes[i].length > 0) {
+                        let firstCharacterMatches = regexIndexes[i][0] === 0;
+                        let endOfFirstPlainText = regexIndexes[i][0];
+                        let startOfLastPlainText = regexIndexes[i][regexIndexes[i].length - 1] + regexs[i][regexIndexes[i].length - 1].length;
+                        
+                        if(!firstCharacterMatches) {
+                            texts[i].push(line.substring(0, endOfFirstPlainText));
+                            textIndexes[i].push(0);
                         }
-                        if(indexes[i].length > 0) {
-                            plainTexts[i].push(line.substring(indexes[i][indexes[i].length - 1] + matchedTexts[i][indexes[i].length - 1].length - 1, line.length - 1));
+                        for(let j = 0; j < regexIndexes[i].length - 1; j++) {
+                            let startOfText = regexIndexes[i][j] + regexs[i][j].length;
+                            let endOfText = regexIndexes[i][j+1];
+                            texts[i].push(line.substring(startOfText, endOfText));
+                            textIndexes[i].push(startOfText);
                         }
+                        if(startOfLastPlainText < line.length) {
+                            texts[i].push(line.substring(startOfLastPlainText, line.length));
+                            textIndexes[i].push(startOfLastPlainText);
+                        }
+                    } else {
+                        texts[i].push(line);
+                        textIndexes[i].push(0);
                     }
                 }
-                console.log(indexes);
-                console.log(matchedTexts);
-                console.log(plainTexts);
+                console.log(regexs);
+                console.log(regexIndexes);
+                console.log(texts);
+                console.log(textIndexes);
                 console.log('');
 
-
+                let mergedTexts : string[][] = [];
+                
             }
         }
     });

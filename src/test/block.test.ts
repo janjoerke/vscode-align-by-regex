c5effe12a138c203d53ed30bbf6c803212677a51
SoftwareApe <carl.muellerroemer@gmail.com>
2019-01-12 19:47:37 +0100

    Improve interpretability of failed test by using non-boolean assertions, add failing test for spacing

--- src/test/block.test.ts
+++ src/test/block.test.ts
@@ -1,4 +1,5 @@
 import { Block } from '../block';
+import { Line } from '../line';
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 
@@ -14,60 +15,60 @@ e..e`;
         let startLine = 0;
         let block : Block = new Block(text, input, startLine, vscode.EndOfLine.LF);
 
-        assert(block.lines.length === 5);
+        assert.deepEqual(block.lines.length, 5);
         
-        assert(block.lines[0].number === 0);
-        assert(block.lines[0].parts.length === 5);
-        // assert(block.lines[0].parts[0].position === 0);
-        assert(block.lines[0].parts[0].value === 'a');
-        // assert(block.lines[0].parts[1].position === 1);
-        assert(block.lines[0].parts[1].value === '.');
-        // assert(block.lines[0].parts[2].position === 2);
-        assert(block.lines[0].parts[2].value === 'a');
-        // assert(block.lines[0].parts[3].position === 3);
-        assert(block.lines[0].parts[3].value === '.');
-        // assert(block.lines[0].parts[4].position === 4);
-        assert(block.lines[0].parts[4].value === 'a');
+        assert.deepEqual(block.lines[0].number, 0);
+        assert.deepEqual(block.lines[0].parts.length, 5);
+        // assert.deepEqual(block.lines[0].parts[0].position, 0);
+        assert.deepEqual(block.lines[0].parts[0].value, 'a');
+        // assert.deepEqual(block.lines[0].parts[1].position, 1);
+        assert.deepEqual(block.lines[0].parts[1].value, '.');
+        // assert.deepEqual(block.lines[0].parts[2].position, 2);
+        assert.deepEqual(block.lines[0].parts[2].value, 'a');
+        // assert.deepEqual(block.lines[0].parts[3].position, 3);
+        assert.deepEqual(block.lines[0].parts[3].value, '.');
+        // assert.deepEqual(block.lines[0].parts[4].position, 4);
+        assert.deepEqual(block.lines[0].parts[4].value, 'a');
         
-        assert(block.lines[1].number === 1);
-        assert(block.lines[1].parts.length === 3);
-        // assert(block.lines[1].parts[0].position === 0);
-        assert(block.lines[1].parts[0].value === 'bb');
-        // assert(block.lines[1].parts[1].position === 2);
-        assert(block.lines[1].parts[1].value === '.');
-        // assert(block.lines[1].parts[2].position === 3);
-        assert(block.lines[1].parts[2].value === 'bb');
+        assert.deepEqual(block.lines[1].number, 1);
+        assert.deepEqual(block.lines[1].parts.length, 3);
+        // assert.deepEqual(block.lines[1].parts[0].position, 0);
+        assert.deepEqual(block.lines[1].parts[0].value, 'bb');
+        // assert.deepEqual(block.lines[1].parts[1].position, 2);
+        assert.deepEqual(block.lines[1].parts[1].value, '.');
+        // assert.deepEqual(block.lines[1].parts[2].position, 3);
+        assert.deepEqual(block.lines[1].parts[2].value, 'bb');
         
-        assert(block.lines[2].number === 2);
-        assert(block.lines[2].parts.length === 3);
-        // assert(block.lines[2].parts[0].position === 0);
-        assert(block.lines[2].parts[0].value === 'ccc');
-        // assert(block.lines[2].parts[1].position === 3);
-        assert(block.lines[2].parts[1].value === '.');
-        // assert(block.lines[2].parts[2].position === 4);
-        assert(block.lines[2].parts[2].value === '');
+        assert.deepEqual(block.lines[2].number, 2);
+        assert.deepEqual(block.lines[2].parts.length, 3);
+        // assert.deepEqual(block.lines[2].parts[0].position, 0);
+        assert.deepEqual(block.lines[2].parts[0].value, 'ccc');
+        // assert.deepEqual(block.lines[2].parts[1].position, 3);
+        assert.deepEqual(block.lines[2].parts[1].value, '.');
+        // assert.deepEqual(block.lines[2].parts[2].position, 4);
+        assert.deepEqual(block.lines[2].parts[2].value, '');
         
-        assert(block.lines[3].number === 3);
-        assert(block.lines[3].parts.length === 3);
-        // assert(block.lines[3].parts[0].position === 0);
-        assert(block.lines[3].parts[0].value === '');
-        // assert(block.lines[3].parts[1].position === 0);
-        assert(block.lines[3].parts[1].value === '.');
-        // assert(block.lines[3].parts[2].position === 1);
-        assert(block.lines[3].parts[2].value === 'ddd');
+        assert.deepEqual(block.lines[3].number, 3);
+        assert.deepEqual(block.lines[3].parts.length, 3);
+        // assert.deepEqual(block.lines[3].parts[0].position, 0);
+        assert.deepEqual(block.lines[3].parts[0].value, '');
+        // assert.deepEqual(block.lines[3].parts[1].position, 0);
+        assert.deepEqual(block.lines[3].parts[1].value, '.');
+        // assert.deepEqual(block.lines[3].parts[2].position, 1);
+        assert.deepEqual(block.lines[3].parts[2].value, 'ddd');
 
-        assert(block.lines[4].number === 4);
-        assert(block.lines[4].parts.length === 5);
-        // assert(block.lines[4].parts[0].position === 0);
-        assert(block.lines[4].parts[0].value === 'e');
-        // assert(block.lines[4].parts[1].position === 1);
-        assert(block.lines[4].parts[1].value === '.');
-        // assert(block.lines[4].parts[2].position === 2);
-        assert(block.lines[4].parts[2].value === '');
-        // assert(block.lines[4].parts[3].position === 2);
-        assert(block.lines[4].parts[3].value === '.');
-        // assert(block.lines[4].parts[4].position === 3);
-        assert(block.lines[4].parts[4].value === 'e');
+        assert.deepEqual(block.lines[4].number, 4);
+        assert.deepEqual(block.lines[4].parts.length, 5);
+        // assert.deepEqual(block.lines[4].parts[0].position, 0);
+        assert.deepEqual(block.lines[4].parts[0].value, 'e');
+        // assert.deepEqual(block.lines[4].parts[1].position, 1);
+        assert.deepEqual(block.lines[4].parts[1].value, '.');
+        // assert.deepEqual(block.lines[4].parts[2].position, 2);
+        assert.deepEqual(block.lines[4].parts[2].value, '');
+        // assert.deepEqual(block.lines[4].parts[3].position, 2);
+        assert.deepEqual(block.lines[4].parts[3].value, '.');
+        // assert.deepEqual(block.lines[4].parts[4].position, 3);
+        assert.deepEqual(block.lines[4].parts[4].value, 'e');
     });
 
     test("Test trim", function() {
@@ -77,21 +78,21 @@ e..e`;
         let startLine = 0;
         let block : Block = new Block(text, input, startLine, vscode.EndOfLine.LF).trim();
 
-        assert(block.lines.length === 2);
+        assert.deepEqual(block.lines.length, 2);
         
-        assert(block.lines[0].number === 0);
-        assert(block.lines[0].parts.length === 5);
-        assert(block.lines[0].parts[0].value === 'a ');
-        assert(block.lines[0].parts[1].value === '.');
-        assert(block.lines[0].parts[2].value === ' a ');
-        assert(block.lines[0].parts[3].value === '.');
-        assert(block.lines[0].parts[4].value === 'a');
+        assert.deepEqual(block.lines[0].number, 0);
+        assert.deepEqual(block.lines[0].parts.length, 5);
+        assert.deepEqual(block.lines[0].parts[0].value, 'a ');
+        assert.deepEqual(block.lines[0].parts[1].value, '.');
+        assert.deepEqual(block.lines[0].parts[2].value, ' a ');
+        assert.deepEqual(block.lines[0].parts[3].value, '.');
+        assert.deepEqual(block.lines[0].parts[4].value, 'a');
         
-        assert(block.lines[1].number === 1);
-        assert(block.lines[1].parts.length === 3);
-        assert(block.lines[1].parts[0].value === '    bb');
-        assert(block.lines[1].parts[1].value === '.');
-        assert(block.lines[1].parts[2].value === 'bb');
+        assert.deepEqual(block.lines[1].number, 1);
+        assert.deepEqual(block.lines[1].parts.length, 3);
+        assert.deepEqual(block.lines[1].parts[0].value, '    bb');
+        assert.deepEqual(block.lines[1].parts[1].value, '.');
+        assert.deepEqual(block.lines[1].parts[2].value, 'bb');
     });
 
     test("Test align", function() {
@@ -100,22 +101,59 @@ e..e`;
         let input = '\\.';
         let startLine = 0;
         let block : Block = new Block(text, input, startLine, vscode.EndOfLine.LF).trim().align();
-
-        assert(block.lines.length === 2);
         
-        assert(block.lines[0].number === 0);
-        assert(block.lines[0].parts.length === 5);
-        assert(block.lines[0].parts[0].value === 'a     ');
-        assert(block.lines[0].parts[1].value === '.');
-        assert(block.lines[0].parts[2].value === ' a ');
-        assert(block.lines[0].parts[3].value === '.');
-        assert(block.lines[0].parts[4].value === 'a');
+        assert.deepEqual(block.lines.length, 2);
         
-        assert(block.lines[1].number === 1);
-        assert(block.lines[1].parts.length === 3);
-        assert(block.lines[1].parts[0].value === '    bb');
-        assert(block.lines[1].parts[1].value === '.');
-        assert(block.lines[1].parts[2].value === 'bb');
+        assert.deepEqual(block.lines[0].number, 0);
+        assert.deepEqual(block.lines[0].parts.length, 5);
+        assert.deepEqual(block.lines[0].parts[0].value, 'a     ');
+        assert.deepEqual(block.lines[0].parts[1].value, '.');
+        assert.deepEqual(block.lines[0].parts[2].value, ' a ');
+        assert.deepEqual(block.lines[0].parts[3].value, '.');
+        assert.deepEqual(block.lines[0].parts[4].value, 'a');
+        
+        assert.deepEqual(block.lines[1].number, 1);
+        assert.deepEqual(block.lines[1].parts.length, 3);
+        assert.deepEqual(block.lines[1].parts[0].value, '    bb');
+        assert.deepEqual(block.lines[1].parts[1].value, '.');
+        assert.deepEqual(block.lines[1].parts[2].value, 'bb');
     });
+    
+    /* spacing at the end of longer lines is a problem, need to correct it. */
+    test("Test align spacing", function() {
+        /* helper function so we can compare line more easily, could be moved somwhere outside if other tests need it */
+        function concatLineParts(line : Line) : string {
+            return line.parts.map(p => p.value).join('');
+        }
 
+        let text = `I'm gonna pop some tags
+Only got twenty dollars in my pocket
+I'm, I'm, I'm hunting, looking for a come up
+This is fucking awesome.`;
+        let input = '\'';
+        let startLine = 0;
+        let block : Block = new Block(text, input, startLine, vscode.EndOfLine.LF).trim().align();
+        
+        assert.deepEqual(block.lines.length, 4);
+        
+        let currentLine = 0;
+        assert.deepEqual(block.lines[currentLine].number, currentLine);
+        assert.deepEqual(block.lines[currentLine].parts.length, 3);
+        assert.deepEqual(concatLineParts(block.lines[currentLine]), 'I                                   \'m gonna pop some tags');
+        
+        currentLine++;
+        assert.deepEqual(block.lines[currentLine].number, 1);
+        assert.deepEqual(block.lines[currentLine].parts.length, 1);
+        assert.deepEqual(concatLineParts(block.lines[currentLine]), 'Only got twenty dollars in my pocket');
+        
+        currentLine++;
+        assert.deepEqual(block.lines[currentLine].number, 2);
+        assert.deepEqual(block.lines[currentLine].parts.length, 7);
+        assert.deepEqual(concatLineParts(block.lines[currentLine]), 'I                                   \'m, I                \'m, I\'m hunting, looking for a come up');
+        
+        currentLine++;
+        assert.deepEqual(block.lines[currentLine].number, 3);
+        assert.deepEqual(block.lines[currentLine].parts.length, 1);
+        assert.deepEqual(concatLineParts(block.lines[currentLine]), 'This is fucking awesome.');
+    });
 });
